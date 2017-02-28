ca885bb5005aa6490c9ef1d9ed6b3b6aebbcb57e
Jeanlouis Rebello <jeanlouisrebello@gmail.com>
2017-02-27 19:29:26 -0500

    Clean code + mobile slideshow

--- js/landing.js
+++ js/landing.js
@@ -12,39 +12,23 @@ var pictures = [
 		'<img id="fullscreen_image" src="Images/landing/10.jpg">'
 ]
 
-var i = 1;
-
 document.addEventListener("DOMContentLoaded", function() {
 
-	
-	var picture = document.getElementById("fullscreen_image");
+	var i = 1;
 	var divpic = $("#fullscreen_div");
-
-	var firstPic = '<img id="fullscreen_image" src="Images/landing/01.jpg">';
 	
 	window.setInterval(function(){
 
 		
 		if (i == pictures.length-1) {
 
-			console.log('nihao');
-
-			for (j = 0; j < pictures.length; j++) {
-				console.log(pictures[j]);
-			}
-
     		i = 0;
 
 		} 
-		console.log('1');
 
-		$("#fullscreen_div").fadeOut(100, function() {
-
-			console.log('2');
+		divpic.fadeOut(100, function() {
 
 			$("#fullscreen_div img:last-child").remove();
-
-			console.log('3');
 
 			divpic.append(pictures[i]);
 
