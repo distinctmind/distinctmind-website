5635de4049a592c6111d4d4d5f4cb5662fd44b08
Jeanlouis Rebello <jeanlouisrebello@gmail.com>
2017-03-10 00:11:13 -0500

    Website Street Images - Updated
    Massive update.
    
    I’m really happy I got my images to properly resize. It was very
    difficult since each image doesn’t have the same aspect ratio. However
    each image has the same width of 500px. I worked with that and the fact
    that all images height would be based on the first image height. I
    wrote a javascript function to calculate a new height for each wrapper
    div that is based on the auto height of the first image. This function
    is called before load time and when window resizes. It looks to be a
    little bit slow, not that much. But its the best solution I’ve found so
    far. Object fit is key to not distorting the pictures.
    
    Next: To change h1 to a jpg so that I can resize it dynamically. Also
    adjust navigation links on first css breakpoint. Lastly, fill the other
    pages with images.

--- js/street.js
+++ js/street.js
@@ -1,4 +1,12 @@
-$(document).ready(function(){
+
+/* Used to continually adjust the fixed height of each div upon window resize */
+/* Note this doesn't change the size of the first div because every other div's height is based on the first one. */
+function adjustHeight() {
+	var currentImageHeight = $("#firstImage").height();
+	$(".container div").css('height', currentImageHeight);
+}
+
+function hoverNavLinks() {
 
 	var streetList = $(".navItems");
 	var streetListAnchor = $(".navItems a");
@@ -12,5 +20,19 @@ $(document).ready(function(){
 		$(this).removeClass("listHoverOn");
 		$(this).find(".anchorNavItems").removeClass("anchorHoverOn");
 	});
+}
 
+/* Called once before the page even finishes loading all elements to make sure it displays correct height */
+adjustHeight();
+
+
+$(document).ready(function(){
+
+	$( window ).resize(function() {
+  		adjustHeight();
+	});
+
+	//Style applied when user hovers the navigation links
+	hoverNavLinks();
+	
 });
