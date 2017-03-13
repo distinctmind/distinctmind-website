57a95ef72cb8f837150bccd424ac094ca929c4a9
Jeanlouis Rebello <jeanlouisrebello@gmail.com>
2017-03-13 00:06:35 -0400

    Big Changes - Content Filter Works!
    I have made multiple media queries and for specific breakpoints, higher
    resolution pictures are put into place.
    
    One of my hardest challenged when I was working on my website last
    summer was to implement a content filtering system into a html page
    that was already very branched out. The modifications were too vast and
    complicated so I just gave up.
    
    This time, my html structure was still simple enough to be able to
    implement a content filtering script. All I had to do was first
    identify when a user clicks on any of the navigation items (Street,
    land, faces…), then get the respective class name and apply a selected
    class to all images that have that class name. Elements who do not have
    the selected class are set to display none.
    
    Because eventually it would be too much to load all images at once, for
    all other tabs I put the image source as a data-src attribute and later
    loaded the img src attribute when a user clicked on that tab.
    
    Next steps is too setup the next and previous buttons to allow user to
    navigate pictures in the full mode and also allow users to press
    keyboard arrows.

--- js/street.js
+++ js/street.js
@@ -3,36 +3,140 @@
 /* Note this doesn't change the size of the first div because every other div's height is based on the first one. */
 function adjustHeight() {
 	var currentImageHeight = $("#firstImage").height();
-	$(".container div").css('height', currentImageHeight);
+	$(".container .image-div").css('height', currentImageHeight);
 }
 
-function hoverNavLinks() {
+var biggerImages = [ 
 
-	var streetList = $(".navItems");
-	var streetListAnchor = $(".navItems a");
+		'../Images/street/medium/01.jpg',
+		'../Images/street/medium/02.jpg',
+		'../Images/street/medium/03.jpg',
+		'../Images/street/medium/04.jpg',
+		'../Images/street/medium/05.jpg',
+		'../Images/street/medium/06.jpg'
+]
 
-	streetList.hover(function() {
+function showModal(mode) {
 
-		$(this).addClass("listHoverOn");
-		$(this).find(".anchorNavItems").addClass("anchorHoverOn");
+	$(".modal" + "." + mode).show();
+	$(".modal" + "." + mode).css('display', 'block');
+	if (mode == "picture") {
+		$(".modalPicture").css('display', 'block');
+	}
+	$("html").css('overflow-y', 'hidden');
+}
 
-	}, function(){
-		$(this).removeClass("listHoverOn");
-		$(this).find(".anchorNavItems").removeClass("anchorHoverOn");
+function closeModal(mode) {
+	$(".modal" + "." + mode).hide();
+	$(".modal" + "." + mode).css('display', 'none');
+	if (mode == "picture") {
+		$(".modalPicture").css('display', 'none');
+	}
+	$("html").css('overflow-y', 'visible');
+}
+
+function changePictures() {
+
+	$(".container div img").each(function(i) {
+		$(this).attr('src', biggerImages[i]);
+	});
+
+}
+
+function showPictures(theFilter) {
+
+	var images = $(theFilter);
+
+	for (var i = 0; i < images.length; i++) {
+		images[i].src = images[i].getAttribute('data-src');
+	}
+
+}
+
+function filterImages() {
+
+	$(".filter-options").click(function(){
+
+		var filter = $(this).children().attr("class");
+		showPictures(".image-div" + "." + filter + " img");
+
+		$("#firstImage").removeAttr("id");
+		var mask = $("#filter-mask");
+		mask.addClass("filter-mask");
+
+		var newSelector = ".image-div." + filter;
+
+		$(newSelector).first().children().attr("id", "firstImage")
+
+		setTimeout(function(){
+			$(".image-div").removeClass("selected");
+			$(newSelector).addClass("selected");
+			closeModal("menu");
+		}, 100);
+
+		setTimeout(function(){
+			$(".filter-mask").removeClass("filter-mask");
+
+		}, 500);
+
 	});
 }
 
+function popPicture(thePicture) {
+	showModal("picture");
+	$("#biggerImage").attr('src', thePicture.attr('data-full-path'));	
+}
+
+function clickPicture() {
+
+	$(".container .image-div img").click(function(){
+		popPicture($(this));
+	});
+
+	$(".modal.picture").click(function(){
+
+		if ($(this) == $("#biggerImage")) {
+		} else {
+			closeModal("picture");
+		}
+		
+	});
+}
 /* Called once before the page even finishes loading all elements to make sure it displays correct height */
 adjustHeight();
 
 
 $(document).ready(function(){
 
-	$( window ).resize(function() {
+	filterImages();
+	clickPicture();
+
+	$(window).resize(function() {
+  		
   		adjustHeight();
+  		
+  		if ($(window).width() <= 917) {
+			$("header .navBar").fadeOut(300);
+
+			if ($(window).width() <= 750) {
+				changePictures();
+			}
+  		} else {
+  			$("header .navBar").fadeIn(300);
+  		}
+  		
 	});
 
-	//Style applied when user hovers the navigation links
-	hoverNavLinks();
-	
+	if ($(window).width() <= 750) {
+		changePictures();
+	}
+
+	$("#burgerIcon").click(function() {
+		showModal("menu")
+	});
+
+	$("#closeModal").click(function() {
+		closeModal("menu");
+	});
+		
 });
